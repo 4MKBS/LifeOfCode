60be54b00a4a9e4ea3d4c7bb73ace7158ef14f46
4mkbs <mkb4sakib@gmail.com>
2025-05-10 14:46:04 +0600

    add include and require in date and time in php

--- Real-Life/php/date&time.php
+++ Real-Life/php/date&time.php
@@ -1,4 +1,6 @@
 <?php
+include "function.php";
+
 
 // date and time
 date_default_timezone_set("asia/dhaka");
@@ -7,3 +9,10 @@ echo "Current date: " . date("Y-m-d") . "\n";
 echo "Current time: " . date("H:i:s") . "\n";
 echo "Current date and time: " . date("Y-m-d H:i:s") . "\n";
 echo "Current timestamp: " . time() . "\n";
+
+echo "\n\n";
+hello("sakib");
+
+echo "\n\n";
+
+require "squareFormula.php";
