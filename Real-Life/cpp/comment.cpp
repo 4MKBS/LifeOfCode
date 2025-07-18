c60e7de436d186ac645be9a9d532a09de215519b
4mkbs <mkb4sakib@gmail.com>
2025-07-18 21:41:29 +0600

    feat: add comment and input-output examples in C++

---
+++ Real-Life/cpp/comment.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Comments in C++
+    // Comments are used to explain code and make it more readable.
+    // They are ignored by the compiler and do not affect the program's execution.
+    // There are two types of comments in C++:
+
+    // Single-line comment
+
+    /*
+       Multi-line comment
+       here you can write multiple lines of comments
+       without needing to use the single-line comment syntax.
+    */
+    int x = 10; // variable x is initialized to 10
+    cout << "That's all about comments in C++!" << endl;
+    return 0;
+}
---
+++ Real-Life/cpp/input-output.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Input and Output in C++
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+    cout << "You entered: " << x << endl;
+    return 0;
+}
