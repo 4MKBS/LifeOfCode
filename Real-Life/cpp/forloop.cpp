496d99ad98e2cbc5836ea0315e5b1b05546e660b
4mkbs <mkb4sakib@gmail.com>
2025-07-21 18:13:50 +0600

    feat: add for loop example in C++

---
+++ Real-Life/cpp/forloop.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // For Loop Example in C++
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << "you are the no. " << i << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
