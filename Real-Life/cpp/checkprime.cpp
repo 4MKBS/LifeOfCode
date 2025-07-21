c03ee7320f13bf680344f0ee3c2f65b441a8960f
4mkbs <mkb4sakib@gmail.com>
2025-07-21 19:24:50 +0600

    feat: add program to check if a number is prime in C++

---
+++ Real-Life/cpp/checkprime.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // program to check if a number is prime in C++
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    bool isPrime = true;
+
+    for (int i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = false;
+            cout << n << " is not a prime number." << endl;
+            break; // Exit the loop if a divisor is found
+        }
+    }
+
+    if (isPrime)
+    {
+        cout << n << " is a prime number." << endl;
+    }
+    return 0;
+}
