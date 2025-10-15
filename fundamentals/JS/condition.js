5b8c3b280dbb71fb5cce6ced274d6b8a9498bad1
4mkbs <mkb4sakib@gmail.com>
2025-10-15 23:14:03 +0600

    feat: add fullscreen text display functionality with dynamic styling options and add condition in js fundamental

---
+++ fundamentals/JS/condition.js
@@ -0,0 +1,9 @@
+let age = 21;
+
+if (age >= 18) {
+  console.log("you are adult");
+} else if (age < 18 && age > 12) {
+  console.log("you are teenager");
+} else {
+  console.log("you are child");
+}
