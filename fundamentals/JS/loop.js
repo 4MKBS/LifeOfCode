25fdd6e3680f319b700abb0336f629ec253e6cd9
4mkbs <mkb4sakib@gmail.com>
2025-10-16 22:23:29 +0600

    feat: add loop examples using for, while, and do-while constructs

---
+++ fundamentals/JS/loop.js
@@ -0,0 +1,14 @@
+for(let i=0; i<10; i++){
+    console.log(`i am ${i}`);
+}
+
+let j=0;
+while(j<10){
+    console.log(`j is ${j}`);
+    j++;
+}
+
+do{
+    console.log(`j is now ${j}`);
+    j++;
+}while(j<10);
