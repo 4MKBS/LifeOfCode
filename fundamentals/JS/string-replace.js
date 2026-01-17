aadab9b8f134bb51c6a653af215e96243a746bf1
4mkbs <mkb4sakib@gmail.com>
2026-01-17 17:29:39 +0600

    feat: add examples for replace and replaceAll methods feat: add examples for includes, startsWith, and endsWith methods

---
+++ fundamentals/JS/string-replace.js
@@ -0,0 +1,4 @@
+// replace and replaceAll
+
+'hello world'.replace('world', 'JS');
+'aaa'.replaceAll('a', 'b');
---
+++ fundamentals/JS/string-search.js
@@ -0,0 +1,6 @@
+// includes, startsWith, endsWith
+
+const phrase = 'hello world';
+phrase.includes('world');
+phrase.startsWith('hell');
+phrase.endsWith('ld');
